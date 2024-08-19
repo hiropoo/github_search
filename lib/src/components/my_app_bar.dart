9756d673bff3a93b760e43ee2f43bb8f675eb6cf
hiropoo <125180473+hiropoo@users.noreply.github.com>
2024-08-19 21:35:48 +0900

    ⚡️ Implement AppBar as a component

---
+++ lib/src/components/my_app_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MyAppBar({super.key, required this.title});
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      centerTitle: true,
+      forceMaterialTransparency: true,
+      scrolledUnderElevation: 0,
+      title: Text(
+        title,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
